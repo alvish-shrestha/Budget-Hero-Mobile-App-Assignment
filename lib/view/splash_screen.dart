854c5be0cf6519f490968402d85f1edb4c49a3d6
alvi-sh <alvishstha97@gmail.com>
2025-05-17 21:18:30 +0545

    Splash Screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -25,6 +25,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(builder: (_) => LoginScreen()),
       );
