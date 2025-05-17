4da47f2943bcdcc2638ce83b79099eb001164f83
alvi-sh <alvishstha97@gmail.com>
2025-05-17 21:22:51 +0545

    git test

---
+++ lib/budget_hero.dart
@@ -0,0 +1,11 @@
+import 'package:budgethero/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class BudgetHero extends StatelessWidget {
+  const BudgetHero({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashScreen(), debugShowCheckedModeBanner: false);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:budgethero/budget_hero.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BudgetHero());
+}
