529cacec373fc029d9aaa8b177436392bf85afe7
alvi-sh <alvishstha97@gmail.com>
2025-05-24 16:15:51 +0545

    conflict

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -259,6 +259,7 @@ class _SignupState extends State<Signup> {
                           );
                           Future.delayed(const Duration(seconds: 1), () {
                             Navigator.pushReplacement(
+                              // ignore: use_build_context_synchronously
                               context,
                               MaterialPageRoute(
                                 builder: (context) => const LoginScreen(),
