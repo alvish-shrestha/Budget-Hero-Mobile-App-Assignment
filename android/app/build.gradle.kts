7478e26e1eecdeeef19ef3163867f44da6924e86
alvi-sh <alvishstha97@gmail.com>
2025-06-17 20:51:45 +0545

    added first name and last name in signup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.alvish.myproject.budgethero"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
---
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String? userId;
+  final String username;
+  final String email;
+  final String password;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
