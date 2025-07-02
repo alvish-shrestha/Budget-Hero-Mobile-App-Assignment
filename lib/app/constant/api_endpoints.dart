e2e81469133e3bb518cdc720f9486a90c4d48788
alvi-sh <alvishstha97@gmail.com>
2025-07-03 00:48:52 +0545

    some changes on api_endpoints

--- lib/app/constant/api_endpoints.dart
+++ lib/app/constant/api_endpoints.dart
@@ -2,8 +2,8 @@ class ApiEndpoints {
   ApiEndpoints._();
 
   // Timeouts
-  static const connectionTimeout = Duration(seconds: 1000);
-  static const receiveTimeout = Duration(seconds: 1000);
+  static const connectionTimeout = Duration(seconds: 10);
+  static const receiveTimeout = Duration(seconds: 10);
 
   // For Android Emulator
   static const String serverAddress = "http://10.0.2.2:5000";
@@ -11,9 +11,9 @@ class ApiEndpoints {
   // static const String serverAddress = "http://localhost:5000";
 
   // For iPhone (uncomment if needed)
-  static const String baseUrl = "$serverAddress/api/auth/";
+  static const String baseUrl = "$serverAddress/api/";
 
   // Auth
-  static const String login = "login";
-  static const String register = "register";
+  static const String login = "auth/login";
+  static const String register = "auth/register";
 }
