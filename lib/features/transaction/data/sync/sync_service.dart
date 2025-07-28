e81be596135606417e2f7f6293316cdac0adecb4
alvi-sh <alvishstha97@gmail.com>
2025-07-29 00:44:47 +0545

    synced

--- lib/features/transaction/data/sync/sync_service.dart
+++ lib/features/transaction/data/sync/sync_service.dart
@@ -26,3 +26,42 @@ class SyncService {
     }
   }
 }
+
+
+// import 'package:budgethero/app/service_locator/service_locator.dart';
+// import 'package:budgethero/core/network/hive_service.dart';
+// import 'package:budgethero/features/home/presentation/view_model/dashboard_event.dart';
+// import 'package:budgethero/features/home/presentation/view_model/dashboard_view_model.dart';
+// import 'package:budgethero/features/transaction/data/data_source/remote_datasource/transaction_remote_datasource.dart';
+// import 'package:flutter/material.dart';
+
+// class SyncService {
+//   final HiveService hiveService;
+//   final TransactionRemoteDatasource remoteDatasource;
+
+//   SyncService({required this.hiveService, required this.remoteDatasource});
+
+//   Future<void> syncPendingTransactions() async {
+//     try {
+//       final unsyncedTxs = await hiveService.getUnsyncedTransactions();
+
+//       for (final tx in unsyncedTxs) {
+//         try {
+//           final entity = tx.toEntity();
+//           await remoteDatasource.addTransaction(entity);
+//           await hiveService.markTransactionAsSynced(tx.id);
+//         } catch (e) {
+//           debugPrint('Failed to sync transaction ${tx.id}: $e');
+//         }
+//       }
+
+//       serviceLocator<DashboardViewModel>().add(
+//         LoadSelectedMonthTransactionsEvent(),
+//       );
+
+//       debugPrint("Sync completed: ${unsyncedTxs.length} transactions");
+//     } catch (e) {
+//       debugPrint("Sync failed: $e");
+//     }
+//   }
+// }
